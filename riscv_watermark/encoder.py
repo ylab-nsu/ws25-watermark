4c1931213aa1f29cfbb3be390e32ef343a5b6d9e
ggnmstr <jora.babayan@gmail.com>
2025-02-08 15:29:26 +0700

    return sum of sizes in encoder.get_nbits()

--- riscv_watermark/encoder.py
+++ riscv_watermark/encoder.py
@@ -7,23 +7,20 @@ logger = logging.getLogger(__name__)
 
 
 class Encoder:
-    def __init__(
-        self, src_filename: str, methods: list[Watermarker], message: str
-    ):
+    def __init__(self, src_filename: str, methods: list[Watermarker], message: str):
         self.src_filename = src_filename
         self.methods = methods
         self.message: str = message
         # calculate total size in bits available to encode in ELF
         self.sizes = [
-            watermarker.get_nbits(self.src_filename)
-            for watermarker in self.methods
+            watermarker.get_nbits(self.src_filename) for watermarker in self.methods
         ]
 
     def total_size(self):
         return sum(self.sizes)
 
     def can_encode(self):
-        return self.total_size() / 8 >= len(self.message.encode('utf-8'))
+        return self.total_size() / 8 >= len(self.message.encode("utf-8"))
         # тут на самом деле не 8. Значение может
         # варьироваться в зависимости от количества замен
 
@@ -31,26 +28,25 @@ class Encoder:
         #        if not self.can_encode():
         #            logger.info('Not enough size to encode')
         #            raise NoSizeException('')
-        new_data = b''
+        new_data = b""
         for watermarker in self.methods:
             number = watermarker.get_nbits(self.src_filename)
             number //= 8
             c = list(self.message)
             msg_len = len(c)
             if number < 1:
-                logger.info('low amount of codeable bits')
+                logger.info("low amount of codeable bits")
             if number < msg_len:
-                logger.info('Not enough bits to encode the whole message')
+                logger.info("Not enough bits to encode the whole message")
             if number > msg_len:
                 for i in range(number - msg_len):
-                    c.append(' ')   # пока так
+                    c.append(" ")  # пока так
             new_data = watermarker.encode(self.src_filename, c)
-        if new_data != b'':
+        if new_data != b"":
             return new_data
         else:
-            logger.info('encoding failed')
+            logger.info("encoding failed")
             sys.exit()
 
-    def getnbits(self):
-        for watermarker in self.methods:
-            return watermarker.get_nbits(self.src_filename)
+    def get_nbits(self):
+        return sum(self.sizes)
--- riscv_watermark/main.py
+++ riscv_watermark/main.py
@@ -12,58 +12,54 @@ logger = logging.getLogger(__name__)
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='RISC-V Watermarker',
-        description='Encode and get messages from RISC-V binaries',
+        prog="RISC-V Watermarker",
+        description="Encode and get messages from RISC-V binaries",
     )
+    parser.add_argument("-e", "--encode", type=str, help="Message to encode in binary")
+    parser.add_argument("-m", "--methods", type=str, help="Specify methods to use")
+    parser.add_argument("-d", "--decode", action="store_true")
     parser.add_argument(
-        '-e', '--encode', type=str, help='Message to encode in binary'
+        "-g",
+        "--get-nbits",
+        action="store_true",
+        help="Print amount of bits available to hide in binary",
     )
-    parser.add_argument(
-        '-m', '--methods', type=str, help='Specify methods to use'
-    )
-    parser.add_argument('-d', '--decode', action='store_true')
-    parser.add_argument(
-        '-g',
-        '--get-nbits',
-        action='store_true',
-        help='Print amount of bits available to hide in binary',
-    )
-    parser.add_argument('filename')
+    parser.add_argument("filename")
     args = parser.parse_args()
 
     if args.encode and args.decode:
-        print('Specify method (encode or decode)')
+        print("Specify method (encode or decode)")
         return
 
-    methods = str(args.methods).split(',')
+    methods = str(args.methods).split(",")
     methods = [fget_watermarker(x) for x in methods]
     if None in methods:
-        logger.info('Unsupported method detected')
+        logger.info("Unsupported method detected")
         return
     if args.get_nbits:
-        enc = Encoder(args.filename, methods, '')
-        nbits = enc.getnbits()
-        print(nbits)   # пока не работает
+        enc = Encoder(args.filename, methods, "")
+        nbits = enc.get_nbits()
+        print(nbits)  # пока не работает
         return
     if args.encode:
         encoder = Encoder(args.filename, methods, args.encode)
         logger.info(encoder.sizes)
-        logger.info('encoding finished')
+        logger.info("encoding finished")
         new_data = encoder.encode()
-        new_filename = args.filename + '.patched'
-        with open(new_filename, 'wb') as f, open(args.filename, 'rb') as fi:
+        new_filename = args.filename + ".patched"
+        with open(new_filename, "wb") as f, open(args.filename, "rb") as fi:
             f.write(fi.read())
             fi.seek(0)
             elfi = ELFFile(fi)
-            textaddr = elfi.get_section_by_name('.text')['sh_addr']
+            textaddr = elfi.get_section_by_name(".text")["sh_addr"]
             f.seek(textaddr)
             f.write(new_data)
     if args.decode:
         decoded = Decoder(args.filename, methods)
         ret_str = decoded.decode()
         print(ret_str)
-        logger.info('decoding finished')
+        logger.info("decoding finished")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
