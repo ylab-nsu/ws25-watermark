d79415472d319b0812efb6b9327af539092f63e4
Egor Bondar <a23134522@icloud.com>
2025-03-23 01:30:09 +0700

    Encoder refactor: docstrings + little refactor to get_nbits and can_encode methods

--- riscv_watermark/encoder.py
+++ riscv_watermark/encoder.py
@@ -7,29 +7,57 @@ logger = logging.getLogger(__name__)
 
 
 class Encoder:
+    """
+    Handles the encoding of messages into binary files using watermarking techniques.
+    
+    This class coordinates the watermarking process using one or more watermarking
+    methods to encode hidden messages into binary files.
+    """
+    
     def __init__(self, src_filename: str, methods: list[Watermarker], message: str):
-        self.src_filename = src_filename
-        self.methods = methods
-        self.message: str = message
-        # calculate total size in bits available to encode in ELF
-        self.sizes = [
-            watermarker.get_nbits(self.src_filename) for watermarker in self.methods
+        """
+        Initialize the encoder with source file, watermarking methods, and message.
+        
+        :param src_filename: Path to the source binary file
+        :type src_filename: str
+        :param methods: List of watermarking instances to use
+        :type methods: list[Watermarker]
+        :param message: Message to encode
+        :type message: str
+        """
+        self.__src_filename = src_filename
+        self.__methods = methods
+        self.__message: str = message
+        
+        self.available_bits_list = [
+            watermarker.get_nbits(self.__src_filename) for watermarker in self.__methods
         ]
+        
+    def get_nbits(self) -> int:
+        """
+        Get the total number of bits available for encoding across all methods.
+        
+        :return: Total number of bits available for encoding
+        """
+        return sum(self.available_bits_list)
 
-    def can_encode(self):
-        return self.get_nbits() / 8 >= len(self.message.encode("utf-8"))
-        # тут на самом деле не 8. Значение может
-        # варьироваться в зависимости от количества замен
+    def can_encode(self) -> bool:
+        """
+        Check if the message can fit in the available capacity.
+        
+        :return: True if the message can be encoded, False otherwise
+        """
+        return self.get_nbits() >= len(self.__message.encode("utf-8")) * 8
 
     def encode(self) -> bytes:
         #        if not self.can_encode():
         #            logger.info('Not enough size to encode')
         #            raise NoSizeException('')
         new_data = b""
-        for watermarker in self.methods:
-            number = watermarker.get_nbits(self.src_filename)
+        for watermarker in self.__methods:
+            number = watermarker.get_nbits(self.__src_filename)
             number //= 8
-            c = list(self.message)
+            c = list(self.__message)
             msg_len = len(c)
             if number < 1:
                 logger.info("low amount of codeable bits")
@@ -38,12 +66,9 @@ class Encoder:
             if number > msg_len:
                 for i in range(number - msg_len):
                     c.append(" ")  # пока так
-            new_data = watermarker.encode(self.src_filename, c)
+            new_data = watermarker.encode(self.__src_filename, c)
         if new_data != b"":
             return new_data
         else:
             logger.info("encoding failed")
             sys.exit()
-
-    def get_nbits(self):
-        return sum(self.sizes)
