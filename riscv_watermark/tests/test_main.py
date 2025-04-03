ac03a9647bcd59d387cecc00e527009c6376fa5f
Coilyun <nikitab4rodkin@yandex.ru>
2025-04-04 00:03:16 +0700

    Add tests for get_available_bits and parse_arguments

--- riscv_watermark/tests/test_main.py
+++ riscv_watermark/tests/test_main.py
@@ -3,7 +3,8 @@ import os
 
 import pytest
 
-from riscv_watermark.main import decode_message, encode_message
+from riscv_watermark.main import decode_message, encode_message, parse_arguments, get_available_bits, \
+    validate_file
 from riscv_watermark.utils import calculate_file_hash
 from riscv_watermark.watermarkers.factory import fget_watermarker
 
@@ -21,11 +22,13 @@ SECRET_MESSAGE = "This file has been signed with ws25-watermark"
 @pytest.mark.parametrize(
     "filepath",
     [
-        "example_bins/sqlite3.elf",
-        "example_bins/example.elf",
+        "../../example_bins/sqlite3.elf",
+        "../../example_bins/example.elf",
     ],
 )
 def test_encode_decode_message(filepath):
+    validate_file(filepath)
+
     directory = os.path.dirname(filepath)
     basic_filepath = os.path.splitext(filepath)[0]
     patched_filepath = os.path.join(directory, f"{os.path.basename(basic_filepath)}.patched")
@@ -66,3 +69,45 @@ def test_encode_decode_message(filepath):
         if os.path.exists(patched_filepath):
             os.remove(patched_filepath)
             logger.info(f"Removed patched file: {patched_filepath}")
+
+@pytest.mark.parametrize(
+    "filepath",
+    [
+        "../../example_bins/sqlite3.elf",
+        "../../example_bins/example.elf",
+    ],
+)
+
+def test_get_available_bits(filepath):
+    logger.info(f"Starting test for get_available_bits with file: {filepath}")
+
+    try:
+        validate_file(filepath)
+
+        watermarker = fget_watermarker("equal_funcs")
+
+        available_bits = get_available_bits(filepath, [watermarker])
+
+        for watermarker_name, capacity in available_bits.items():
+            logger.info(f"Available bits for {watermarker_name}: {capacity} ({capacity // 8} characters)")
+
+        assert all(capacity > 0 for capacity in available_bits.values()), "No available bits for watermarking."
+
+        logger.info(f"Test passed for get_available_bits with file: {filepath}")
+
+    except Exception as e:
+        pytest.fail(f"Test failed due to error: {e}")
+
+
+def test_parse_arguments(monkeypatch):
+    monkeypatch.setattr('sys.argv',
+                        ['riscv-watermark', '-e', 'Hello', '-m', 'method_name', '-d', '-g', '-o', 'output.txt', 'elf_file'])
+
+    args = parse_arguments()
+
+    assert args.encode == 'Hello'
+    assert args.methods == 'method_name'
+    assert args.decode is True
+    assert args.get_nbits is True
+    assert args.output == 'output.txt'
+    assert args.filename == 'elf_file'
