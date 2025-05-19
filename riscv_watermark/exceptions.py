e002630b396bceb6386a5a8e0d6c19352047f028
Egor Bondar <a23134522@icloud.com>
2025-05-20 03:42:43 +0700

    Project Structure Wipe and New Folder Structure with preserving for now old EquilInstructionWatermarker

--- riscv_watermark/exceptions.py
+++
@@ -1,6 +0,0 @@
-class WatermarkError(Exception):
-    """Base exception for all watermarking errors."""
-
-    def __init__(self, message="An error occurred in the watermarking process"):
-        self.message = message
-        super().__init__(self.message)
