9fd52e8601eb1d8065714f08838b8cf7be76ffd6
Egor Bondar <a23134522@icloud.com>
2025-05-22 01:12:23 +0700

    TextSectionHandler tests + Architecture Enum EM_386 -> EM_X86_64

--- watermark_framework/architecture.py
+++ watermark_framework/architecture.py
@@ -5,7 +5,7 @@ from capstone import (
     CS_ARCH_ARM64,
     CS_ARCH_RISCV,
     CS_ARCH_X86,
-    CS_MODE_32,
+    CS_MODE_64,
     CS_MODE_ARM,
     CS_MODE_RISCV32,
     CS_MODE_RISCV64,
@@ -21,10 +21,10 @@ class Architecture(Enum):
     - arch_name: Human-readable architecture name.
     - capstone_arch: Capstone architecture constant.
     - capstone_mode: Capstone mode constant (or combined modes).
-    - e_machine: ELF machine type string identifier(e.g., 'EM_386').
+    - e_machine: ELF machine type string identifier(e.g., 'EM_RISCV').
     - elf_class: ELF class (32 or 64, or None for flexible architectures).
     """
-    I386 = ("i386", CS_ARCH_X86, CS_MODE_32, 'EM_386', 32)
+    X86_64 = ("X86_64", CS_ARCH_X86, CS_MODE_64, 'EM_X86_64', 64)
     RISCV = ("riscv", CS_ARCH_RISCV, CS_MODE_RISCV32 | CS_MODE_RISCV64 | CS_MODE_RISCVC, 'EM_RISCV', None)
     ARM64 = ("arm64", CS_ARCH_ARM64, CS_MODE_ARM, 'EM_AARCH64', 64)
 
@@ -42,7 +42,7 @@ class Architecture(Enum):
         Match ELF header values to an architecture.
 
         Args:
-            e_machine: ELF machine string (e.g., 'EM_386').
+            e_machine: ELF machine string (e.g., 'EM_RISCV').
             elf_class: ELF class (32 or 64).
 
         Returns:
